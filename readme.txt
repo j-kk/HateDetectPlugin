fb42d5ebde555bf5670016de8f50130abeea394e
j-kk <32602977+j-kk@users.noreply.github.com>
2021-09-27 23:55:48 -0400

    Added readme, code cleanup

---
+++ readme.txt
@@ -0,0 +1,32 @@
+=== HateDetect ===
+Contributors: qqkk
+Tags: hatedetect, hatecheck, comments, hate detection, hate, hate-detection, antihate, anti hate, anti-hate
+Requires at least: 4.7
+Tested up to: 5.8
+Stable tag: 0.1
+License: GPLv2 or later
+
+HateDetect checks your comments using the codeagainsthate service to see if they contain hate speech or not.
+
+== Description ==
+
+HateDetect checks your comments using the [codeagainsthate](codeagainsthate.eu) service to see if they contain hate speech or not.
+Provides easy access to see which comments contain hate and lets you automatize comments approval or rejection after
+hate detection.
+In case when comment contains hate, it provides an explanation, why comment was marked.
+The explanation may be shown to the user after submitting the comment (administrator can decide).
+
+HateDetect plugin is currently in the beta phase. There are no fees for using the hate detection api, however please contact
+us to obtain an access code. It's required to use the plugin.
+
+
+== Frequently Asked Questions ==
+
+= Are there any limitations about the user traffic? =
+
+Each site has a reasonable api usage limitations. We will contact you if your site produces too much load on our side.
+
+== Changelog ==
+
+= 0.1 =
+* Basic version of the plugin
--- views/enter.php
+++ views/enter.php
@@ -2,11 +2,14 @@
     <div class="hatedetect-enter-api-key-box centered">
         <div class="enter-api-key">
             <form action="<?php echo esc_url( Hatedetect_Admin::get_page_url() ); ?>" method="post">
-                <?php wp_nonce_field( Hatedetect_Admin::NONCE ) ?>
+				<?php wp_nonce_field( Hatedetect_Admin::NONCE ) ?>
                 <input type="hidden" name="action" value="enter-key">
                 <p style="width: 100%; display: flex; flex-wrap: nowrap; box-sizing: border-box;">
-                    <input id="key" name="key" type="text" size="15" value="" placeholder="<?php esc_attr_e( 'Enter your API key' , 'hatedetect' ); ?>" class="regular-text code" style="flex-grow: 1; margin-right: 1rem;">
-                    <input type="submit" name="submit" id="submit" class="hatedetect-button" value="<?php esc_attr_e( 'Connect with API key', 'hatedetect' );?>">
+                    <input id="key" name="key" type="text" size="15" value=""
+                           placeholder="<?php esc_attr_e( 'Enter your API key', 'hatedetect' ); ?>"
+                           class="regular-text code" style="flex-grow: 1; margin-right: 1rem;">
+                    <input type="submit" name="submit" id="submit" class="hatedetect-button"
+                           value="<?php esc_attr_e( 'Connect with API key', 'hatedetect' ); ?>">
                 </p>
                 <br>
                 <p>
